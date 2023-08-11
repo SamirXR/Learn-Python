96afe29568ae7fe0d026ccc664847a9cefff6062
Samir khan <66723516+SamirXR@users.noreply.github.com>
2023-08-11 16:23:38 +0530

    Create day1.py

---
+++ day1.py
@@ -0,0 +1,56 @@
+# Python
+
+# Introduction to Python
+
+# Python is a Programming Language having Multiple of Predefined Models,Functions,Methods,Classes.A Versatile languages used in Multiple Popular Domains.
+
+
+#  1) Easy to Learn and Use
+#  2) Versatile Domains:
+#  - Data Driven : DA,ML,AI,DS,NLP,IP Etc
+#  - Non-Data Driven : Web Development,Ethical Hacking Etc
+#  3) Interpreted Language
+#  4) OOPS : Object-Oriented Programming language
+#  5) Open Source
+#  6) Python have very big community of Developers.
+#  7) There are Plenty of Libraries and Packages you and install and use.
+#  8) Python is Platform Independent
+#  9) Portable : Suppose if we write some code on our Operating System it can be Executed on Another Operating systems.
+# 10) Python Supports modular Approach of Programming : We divide our Projects in Several Small modules and can finally interface all these modules together.
+# 11) Python supports Dynamic data type definition and Dynamic Data Allocation.
+#  - Dynamic Data type Definition : Definition and Runtime.
+#  - Dynamic Memory Allocation : Memory Allocation at Run time.
+
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+# Downloading Python
+
+# Python : https://www.python.org/downloads/
+# Official Documentation : https://docs.python.org/3/
+
+# Step 1) Visit Official Site: Go to https://www.python.org/.
+# Step 2) Choose Version: Pick the latest Python version.
+# Step 3) Select OS: Click on the link for your operating system (Windows/macOS/Linux).
+# Step 4) Download Installer: Get the installer (exe/dmg) file.
+# Step 5) Run Installer: Double-click to run it and follow instructions.
+# Step 6) Add to PATH (Windows): Check "Add Python to PATH".
+# Step 7) Verify: Open terminal/Command Prompt, type python --version.
+
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+# Where Can we Run/Code Python?
+
+# IDE : integrated Development Environment.
+
+# Pycharm : https://www.jetbrains.com/pycharm/
+# Visual Studio Code : https://code.visualstudio.com/download
+# Anaconda : https://www.anaconda.com/download
+
+
+# (Or if your Device have Low Specs,You can alternatively use some Web Based IDE)
+
+# Web Based IDE : integrated Development Environment on Web (Chrome,Safari,Firefox etc.)
+
+# Replit : https://replit.com/
+# Google Colab : https://colab.research.google.com/
+
+
+
